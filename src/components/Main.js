1e3eaf80ec9603e47113ca2212b01fd5cb309bc9
darocygaro <dariusz.wodecki@tieto.com>
2020-12-31 01:41:56 +0100

    Draft menu and styles applied

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import logo from '../logo.svg';
+import '../styles/Main.css';
+import Home from './Home.js'
+import Menu from './Menu.js'
+import UsersList from './UsersList.js'
+import { Switch, Route, HashRouter } from 'react-router-dom';
+
+function Main() {
+    return (
+        <div className="App">
+            <Menu />
+            <HashRouter>
+                <Switch>
+                    <Route
+                        exact
+                        path="/home"
+                        render={(props) =>
+                            <Home />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/userslist"
+                        render={(props) =>
+                            <UsersList />
+                        }
+                    />
+                </Switch>
+            </HashRouter>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/UsersList.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const UsersList = (props) => {
+
+    return (
+        <div>
+            <h1>Users list page...</h1>
+        </div>
+    )
+};
+
+export default UsersList;
