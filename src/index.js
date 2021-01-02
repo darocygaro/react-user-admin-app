039702ef496c9ba02a8713fefcca03d16512d63c
darocygaro <dariusz.wodecki@tieto.com>
2021-01-03 00:53:46 +0100

    jquery removed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
